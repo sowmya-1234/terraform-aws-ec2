35c77ba049e128215dde12522c198fd0ab5ec200
Venugopal <sowmya.venugopal@epsilon.com>
2020-12-16 12:31:03 +0530

    Initial CommitNew

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "demo01" {
   ami           = var.ami
   instance_type = var.instance_type
   tags = {
-    Name = "MANDEEP-EC2"
+    Name = "SOWMYA-EC2"
   }
   key_name = var.keyname
   count = var.noOfInstances
